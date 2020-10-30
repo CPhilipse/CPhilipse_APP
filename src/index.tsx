d4f3f1f84e72e368c37332b46001d47fc1f1b347
Clemens <clemens@machelp.nl>
2020-10-30 12:52:48 +0100

    Configuring React-Navigation, Reanimated and other packages. :wrench:

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-
+import {NavigationContainer} from '@react-navigation/native';
 export default () => {
   return (
-    <View style={{flex: 1}}>
-      <Text>My own APP!!</Text>
-    </View>
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
   );
 };
