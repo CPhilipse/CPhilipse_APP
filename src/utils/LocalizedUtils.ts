0221f2b3dfc447db5905344a9ab037b0edb73c68
Clemens <clemens@machelp.nl>
2020-11-18 21:43:08 +0100

    Fix TS error(:ambulance:): Get rid of some @ts-ignores.

--- src/utils/LocalizedUtils.ts
+++ src/utils/LocalizedUtils.ts
@@ -7,21 +7,41 @@ const languages = {
   nl: 'nl',
 };
 
+// TODO: Research what this piece of TS does and comment explanation
+const getKeyValue = <T extends object, U extends keyof T>(obj: any) => (
+  page: string,
+  value: string,
+) => obj[page][value];
+
 const getLocalizedString = (page: string, value: string) => {
   const state = store.getState();
   const {language} = state.language;
 
-  // @ts-ignore
-  const enText = en[page][value];
-  // @ts-ignore
-  const nlText = nl[page][value];
-  if (language === languages.en) {
-    return enText;
-  } else if (language === languages.nl) {
-    return nlText;
-  } else {
-    return enText;
+  const enText = getKeyValue(en)(page, value);
+  const nlText = getKeyValue(nl)(page, value);
+  switch (language) {
+    case languages.nl:
+      nlText;
+      break;
+    case languages.en:
+      enText;
+      break;
+    default:
+      enText;
+      break;
   }
+
+  // // @ts-ignore
+  // const enText = en[page][value];
+  // // @ts-ignore
+  // const nlText = nl[page][value];
+  // if (language === languages.en) {
+  //   return enText;
+  // } else if (language === languages.nl) {
+  //   return nlText;
+  // } else {
+  //   return enText;
+  // }
 };
 
 export {getLocalizedString, languages};
