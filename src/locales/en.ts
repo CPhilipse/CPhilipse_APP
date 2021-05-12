83632fdd5263ba97df2278097461784620652836
Clemens <clemens@machelp.nl>
2021-05-12 17:02:14 +0200

    General(:zap:): translations.

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -36,15 +36,15 @@ export default {
     fourthCard: ':https://github.com/CPhilipse/CPhilipse_APP',
   },
   [Pages.PROJECT_DETAILS]: {
-    overlayCopy:
-      'The first image shows the homepage of this app. This was an' +
-      'interesting page to make, because of the splashscreen and filters.' +
-      'The splashscreen was the hardest part, though I made it from a' +
-      'tutorial, making the animation work with my own custom text was a' +
-      "bit of a hassle. The text is made out of SVG and I've like no" +
-      'experience with SVG. When I tried it the first time with my own text' +
-      "as SVG, it didn't animate the whole text. It only animated half of" +
-      'the letters. After some trial and error I found the correct number' +
-      'to adjust to have it show correctly.',
+    portfolio_app:
+      'Background\nI wanted to make my own app with a lot of fancy animations. So I started making this app. This app does not have the amount of fancy animations as I initially had in mind. I started to drift away from making apps in general when I started my new education, which does not have a lot do to with making apps. Despite of that, I really enjoyed making this app, I think it looks beautiful and it does have some really nice animations.\n\nThe app\nThe photos are the screens you can visit in the app. The page I am most proud of is the home page. This page has the splashscreen with the nice animation, the filter and just a beautiful design.\n\nDuration\nIt took me approximately 7 months to finish this app from start to end.',
+    hvgea_app:
+      'Background\nMy aunt sent me a screenshot of a post on Facebook saying that someone of the church is looking for someone who can build an app. It sounded interesting, so I contacted this person, we met up and discussed the app. He wanted a blog to reach out to people more easily for the people who attend his organized prayer nights.\n\nThe app\nThis app is a blog which contains a full login system, splashscreen, comments on each article, multiple categories, feedback page, library, settings, the admin has its own post screen on which the admin can style and customize the text with bold, striked, linking to a different page etc., the admin can make books from within the app, delete comments/feedback of users, make posts with multiple images. There is a notifications system and users can reserve books.\nThis was quite a big app to make.\n\nDuration\nIt took me about 1 year to finish this app.',
+    exam_app:
+      "Background\nFor my previous education on the MBO (secondary professional certificate), I had to make an app to wrap up an important module. I believe I was given about 3 weeks to make this app with the required preparation rapports and we were teached making an app for like 1 month.\n\nThe app\nAs an user you should be able to make an incident through the app as to which you can notify certain people with through email. When opening the app, there'll be a button to open the camera. When making the photo, you'll go to the form where the photo is being displayed. On this form the location is automatically being added. You can describe the incident and categorize this incident in five categories. Then there will be an option to mail this incident or push this incident, which will save the data locally.\n\nDuration\nIt took me the full 3 weeks to make this app.",
+    internship_copy:
+      "Background\nFor my school I had to look for a company to do my internship. I didn't known any companies that worked on mobile apps, so my teacher recommended among others Media Monks. So I got an interview there and that is how I got in.\n\nThe apps\nThey let me work on multiple apps, which was awesome, because they were big apps. I am talking New York Pizza, ACE Hardware, Gildan Product Locator, Google Home and the intern app of Media Monks. To conclude, this was an awesome internship.\n\nDuration\nThis internship went for about 4 months.",
+    iwder_web:
+      "Background\nFor the education that I am doing currently, I had to wrap a module by making a webshop that gives the illusion that it works.\n\nThe website\nThe website had to be about a game or serie. I don't recall on the reason why I chose Mario, but I think the finished product looks pretty nice! All this according to the W3C rules, so this whole website is without errors and with clean code according to the general guidelines. This website is made fully in HTML, CSS and JS.\n\nDuration\nI believe it took me around 2 weeks to make this website.",
   },
 };
--- src/utils/DummyData.ts
+++ src/utils/DummyData.ts
@@ -1,6 +1,8 @@
 import {ImageRequireSource} from 'react-native';
 import Categories from '../enum/Categories';
 import {images} from '../themes';
+import {getLocalizedString} from './LocalizedUtils';
+import Pages from '../enum/Pages';
 
 export interface ProjectProps {
   id: number;
@@ -13,14 +15,15 @@ export interface ProjectProps {
   category: string;
 }
 
+const localizedCopy = (value: string) => getLocalizedString(Pages.HOME, value);
+
 export const projects = [
   {
     id: 0,
     home_image: images.app_icon,
     title: 'Portfolio App',
     subTitle: 'Welcome to my very own app!',
-    body:
-      'Background\nI wanted to make my own app with a lot of fancy animations. So I started making this app. This app does not have the amount of fancy animations as I initially had in mind. I started to drift away from making apps in general when I started my new education, which does not have a lot do to with making apps. Despite of that, I really enjoyed making this app, I think it looks beautiful and it does have some really nice animations.\n\nThe app\nThe photos are the screens you can visit in the app. The page I am most proud of is the home page. This page has the splashscreen with the nice animation, the filter and just a beautiful design.\n\nDuration\nIt took me approximately 7 months to finish this app from start to end.',
+    body: localizedCopy('portfolio_app'),
     images: [
       images.app_home,
       images.app_menu,
@@ -40,8 +43,7 @@ export const projects = [
     home_image: images.hvgea_icon,
     title: 'HvGeA',
     subTitle: 'Home of Prayer and Worship.',
-    body:
-      'Background\nMy aunt sent me a screenshot of a post on Facebook saying that someone of the church is looking for someone who can build an app. It sounded interesting, so I contacted this person, we met up and discussed the app. He wanted a blog to reach out to people more easily for the people who attend his organized prayer nights.\n\nThe app\nThis app is a blog which contains a full login system, splashscreen, comments on each article, multiple categories, feedback page, library, settings, the admin has its own post screen on which the admin can style and customize the text with bold, striked, linking to a different page etc., the admin can make books from within the app, delete comments/feedback of users, make posts with multiple images. There is a notifications system and users can reserve books.\nThis was quite a big app to make.\n\nDuration\nThere is a lot to say about this app and my progress, but I am leaving it here. It took me about 1 year to finish this app.',
+    body: localizedCopy('hvgea_app'),
     images: [images.hvgea],
     video: 'video',
     category: Categories.BLOG,
@@ -52,8 +54,7 @@ export const projects = [
     title: 'Exam',
     subTitle:
       'Mobile Application Development (MAD). Creating detailed incident reports.',
-    body:
-      "Background\nFor my previous education on the MBO (secondary professional certificate), I had to make an app to wrap up an important module. I believe I was given about 3 weeks to make this app with the required preparation rapports and we were teached making an app for like 1 month.\n\nThe app\nAs an user you should be able to make an incident through the app as to which you can notify certain people with through email. When opening the app, there'll be a button to open the camera. When making the photo, you'll go to the form where the photo is being displayed. On this form the location is automatically being added. You can describe the incident and categorize this incident in five categories. Then there will be an option to mail this incident or push this incident, which will save the data locally.\n\nDuration\nIt took me the full 3 weeks to make this app.",
+    body: localizedCopy('exam_app'),
     images: [images.cphilipse],
     video: 'video1',
     category: Categories.SCHOOL,
@@ -62,8 +63,8 @@ export const projects = [
     id: 3,
     home_image: images.android_default_icon,
     title: 'Internship',
-    subTitle: 'All the apps I worked on within my second internship.',
-    body: 'body2',
+    subTitle: 'All the apps I worked on within my internship.',
+    body: localizedCopy('internship_copy'),
     images: [images.hvgea],
     video: 'video2',
     category: Categories.SCHOOL,
@@ -73,17 +74,7 @@ export const projects = [
     home_image: images.android_default_icon,
     title: 'IWDER Module',
     subTitle: 'A website I made in HTML, CSS and JS to wrap up a module.',
-    body: 'body2',
-    images: [images.hvgea],
-    video: 'video2',
-    category: Categories.SCHOOL,
-  },
-  {
-    id: 5,
-    home_image: images.android_default_icon,
-    title: 'First internship',
-    subTitle: 'The websites I worked on within my first internship.',
-    body: 'body2',
+    body: localizedCopy('iwder_web'),
     images: [images.hvgea],
     video: 'video2',
     category: Categories.SCHOOL,
